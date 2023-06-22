5bdaabacdb8606ebf24b0866add6514636b1adf3
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-22 01:45:39 -0600

    Spell Correction

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <PortafolioApp />  {/* Esto se va a renderizar */}
+     <PortafolioApp /> {/* Esto se va a renderizar */}
   </React.StrictMode>
 );
 
