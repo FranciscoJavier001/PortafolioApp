bfafd6676f5cff071ecd500a71fabf281d73a318
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-22 02:17:01 -0600

    ProyectosScreen Layout And Annotations

---
+++ src/components/proyectos/ProyectosScreen.js
@@ -0,0 +1,9 @@
+import React from 'react' //* Importamos React */
+
+export const ProyectosScreen = () => { //* Exportamos la variable de este proyecto */
+    return ( //* Lo que voy a retornar */
+        <div> {/* Una Etiqueta */}
+            <h1>ProyectosScreen</h1> {/* Un Titulo */}
+        </div>
+    )
+}
