44741396f495cf95490d21f1dbafe1856d5e8347
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-22 01:47:32 -0600

    Render In Dom PortafolioApp

---
+++ src/PortafolioApp.js
@@ -0,0 +1,9 @@
+import React from 'react' //* Hay que importarla de REact */
+
+export const PortafolioApp = () => { //* Exportamos la variable que queremos utilizar en otros lugares */
+    return ( //* Lo que voy a Devolver */
+        <div> {/* Simplemente es una etiqueta sin mas ni clases */}
+            <h1>PortafolioApp</h1> {/* Es un Titulo en la pantalla */}
+        </div>
+    )
+}
