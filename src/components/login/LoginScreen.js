c25c741d5eed05edf4949ea5548207beca47a83b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-22 02:14:35 -0600

    LoginScreen Layout And Annotations

---
+++ src/components/login/LoginScreen.js
@@ -0,0 +1,9 @@
+import React from 'react' //* Invoco a React */
+
+export const LoginScreen = () => { //* Variable a Exportar */
+    return ( //* Lo que va a retornar */
+        <div> {/* Una Etiqueta */}
+            <h1>LoginScreen</h1> {/* Un Titulo */}
+        </div>
+    )
+}
