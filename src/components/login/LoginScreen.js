ed7581e91c1cc7edabb32383827d04aac922b6f5
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-26 19:01:49 -0600

    Delete File

--- src/components/login/LoginScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react' //* Invoco a React */
-
-export const LoginScreen = () => { //* Variable a Exportar */
-    return ( //* Lo que va a retornar */
-        <div> {/* Una Etiqueta */}
-            <h1>LoginScreen</h1> {/* Un Titulo */}
-        </div>
-    )
-}
