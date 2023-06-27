02a248fe37a0754b127de083b9edefde455b7591
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-26 19:01:14 -0600

    New Route to Render in Main App

--- src/PortafolioApp.js
+++ src/PortafolioApp.js
@@ -1,9 +1,10 @@
 import React from 'react' //* Hay que importarla de React */
+import { AppRouter } from './routers/AppRouter' //* Necesitamos Importarla, porque vamos a mostrarla */
 
 export const PortafolioApp = () => { //* Exportamos la variable que queremos utilizar en otros lugares */
     return ( //* Lo que voy a Devolver */
         <div> {/* Simplemente es una etiqueta sin mas ni clases */}
-            <h1>PortafolioApp</h1> {/* Es un Titulo en la pantalla */}
+            <AppRouter/> {/* La App que vamos a Renderizar */}
         </div>
     )
 }
